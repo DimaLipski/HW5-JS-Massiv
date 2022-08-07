aee2e1afb323ddbb9cd003b494e0e21f7402a9ad
Dima Lipski <lipsdima@yandex.ru>
2022-08-07 17:30:19 +0300

    JS-massiv

---
+++ js/main.js
@@ -0,0 +1,47 @@
+"user strict"
+// Task 1 
+
+// Task 2
+const animals = ['monkey', 'dog', 'cat'];
+console.log(animals.pop());
+
+const numbers = [5, 43, 63, 23, 90];
+// delete numbers[0];
+// delete numbers[1];
+// delete numbers[2];
+// delete numbers[3];
+// delete numbers[4];
+console.log(numbers);
+// Task 2
+// const animals = ['monkey', 'dog', 'cat'];
+// console.log(animals.pop());
+// Task 3
+// const numbers = [5, 43, 63, 23, 90];
+
+// // numbers.pop()
+// // numbers.pop()
+// // numbers.pop()
+// // numbers.pop()
+// // numbers.pop()
+
+// // numbers.splice(0,5)
+// numbers.pop(2)
+// console.log(numbers);
+
+// Task 4
+// const students = ['Polina', 'Dasha', 'Masha'];
+// students.splice(2, 1, `Borya`,)
+// students.splice(0, 1, `Andrey`,)
+// console.log(students);
+
+// // Task 6
+// const evenNumbers = [2, 4, 6, 8, 10];
+// const oddNumbers = [1, 3, 5, 7, 9];
+// const numbers = evenNumbers.concat(oddNumbers);
+// console.log(numbers);
+// console.log(numbers.indexOf('8'));
+
+// Task 7
+const binary = [0, 0, 0, 0];
+const one = binary.join('1');
+console.log(one);
